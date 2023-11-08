c639936e6a7ddd1a480cac2ff5be4e51d6fdd20e
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2023-11-08 14:26:17 -0800

    fixed up tables slightly, still not done

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -7,56 +7,51 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS titles;
 DROP TABLE IF EXISTS departments;
 
+CREATE TABLE "titles" (
+    "title_id" INTEGER PRIMARY KEY NOT NULL,
+    "title" VARCHAR(30) NOT NULL
+);
+
 CREATE TABLE "employees" (
-    "emp_no" INTEGER   NOT NULL,
-    "title_id" INTEGER   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(30)   NOT NULL,
-    "last_name" VARCHAR(30)   NOT NULL,
-    "sex" CHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+    "emp_no" INTEGER PRIMARY KEY NOT NULL,
+    "title_id" VARCHAR(30) NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "sex" CHAR NOT NULL,
+    "hire_date" DATE NOT NULL,
+	foreign key (title_id) references titles (title_id)
 );
 
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR(30)   NOT NULL,
-    "dept_name" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+    "dept_no" VARCHAR(30) PRIMARY KEY NOT NULL,
+    "dept_name" VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE "dept_emp" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no"
-     )
+    "emp_no" INTEGER PRIMARY KEY NOT NULL,
+    "dept_no" VARCHAR(30) NOT NULL,
+    foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no)
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(30)   NOT NULL,
-    "emp_no" INTEGER   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    "dept_no" VARCHAR(30) PRIMARY KEY NOT NULL,
+    "emp_no" INTEGER NOT NULL,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no)
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" INTEGER   NOT NULL,
-    "salary" INTEGER   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+    "emp_no" INTEGER PRIMARY KEY NOT NULL,
+    "salary" INTEGER NOT NULL,
+	foreign key (emp_no) references employees (emp_no)
 );
 
-CREATE TABLE "titles" (
-    "title_id" INTEGER   NOT NULL,
-    "title" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
 
