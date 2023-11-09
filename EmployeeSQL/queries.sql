cd69ce6fb08f5a57fc29e8a5eb31690461b12904
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2023-11-08 17:13:13 -0800

    fourth query complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -16,6 +16,10 @@ INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
 INNER JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT dept_emp.dept_no, dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
