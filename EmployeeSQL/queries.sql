882540b9405e342f34459272745f4ff38b27a5fa
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2023-11-08 17:34:34 -0800

    sixth query complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -27,6 +27,11 @@ FROM employees
 WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%';
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT departments.dept_name, dept_emp.emp_no, employees.last_name, employees.first_name
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name LIKE 'Sales';
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
